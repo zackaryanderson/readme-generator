0813825d6ee9341e88a0ac658e72d6d368db256d
zanderson1998 <zanderson1998@yahoo.com>
2021-01-18 20:03:21 -0700

    finish question prompts and function calls to make markdown

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const promptUser = () => {
         {
             type: 'input',
             name: 'description',
-            message: 'Enter a description of your project? (Required)',
+            message: 'Enter a description of your project: (Required)',
             validate: nameInput => {
                 if (nameInput){
                     return true;
@@ -34,16 +34,72 @@ const promptUser = () => {
             }
         },
         {
-            type: 'checkbox',
+            type: 'input',
+            name: 'githubRepo',
+            message: 'Enter the GitHub Repo: (Required)',
+            validate: nameInput => {
+                if (nameInput){
+                    return true;
+                } else {
+                    console.log('Please enter the GitHub Repo');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'githubUsername',
+            message: 'Enter your GitHub username: (Required)',
+            validate: nameInput => {
+                if (nameInput){
+                    return true;
+                } else {
+                    console.log('Please enter your username');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'list',
             name: 'license',
             message: 'What license does your project use? (Required)',
-            choices: ['MIT','GNU AGPLv3','Mozilla Public License 2.0','Apache License 2.0']
+            choices: ['MIT','Unlicense']
+        },
+        {
+            type: 'confirm',
+            name: 'confirmQuestions',
+            message: 'Would you like to include a "Contact Me" section?',
+            default: false
+        },
+        {
+            type: 'input',
+            name: 'questions',
+            message: 'Please enter your Email (Your GitHub username will also be displayed)',
+            when: ({ confirmQuestions }) => {
+                if (confirmQuestions) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'questionsDescription',
+            message: 'Please explain how you prefer users to contact you:',
+            when: ({ confirmQuestions }) => {
+                if (confirmQuestions) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
         },
         {
             type: 'confirm',
             name: 'confirmInstallation',
             message: 'Would you like to include a description of how to install your project?',
-            default: true
+            default: false
         },
         {
             type: 'input',
@@ -55,12 +111,90 @@ const promptUser = () => {
                 } else {
                     return false;
                 }
+            }
+        },
+        {
+            type: 'confirm',
+            name: 'confirmUsage',
+            message: 'Would you like to include a description of how to use your project?',
+            default: false
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Provide instructions and examples for use:',
+            when: ({ confirmUsage }) => {
+                if (confirmUsage) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'confirm',
+            name: 'confirmCollaborators',
+            message: 'Would you like to include a list of collaborators?',
+            default: false
+        },
+        {
+            type: 'input',
+            name: 'collaborators',
+            message: 'Provide collaborator(s) GitHub profile(s):',
+            when: ({ confirmCollaborators}) => {
+                if (confirmCollaborators) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'confirm',
+            name: 'confirmBadges',
+            message: 'Would you like to include badges?',
+            default: true
+        },
+        {
+            type: 'confirm',
+            name: 'confirmTests',
+            message: 'Would you like to include a list of tests?',
+            default: false
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'Please provide a list of detailed tests to perform:',
+            when: ({ confirmTests}) => {
+                if (confirmTests) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
         },
     ])
 };
 
+
+const writeFile = fileContent => {
+    return new Promise((resolve,reject) => {
+        fs.writeFile('./dist/README.md',fileContent, err => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve({
+                ok: true,
+                message: 'README.md Created.'
+            });
+        });
+    });
+};
+
 promptUser()
-.then(answers => console.log(answers));
+.then(answers => generateMarkdown(answers))
+.then(markdown => writeFile(markdown));
 // // TODO: Create a function to write README file
 // function writeToFile(fileName, data) {}
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,9 +1,8 @@
 const generateContributing = require('./generateContributing.js');
 
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
+//create license badge
 const renderLicenseBadge = (license) => {
-  if (!license){
+  if (!license) {
     return '';
   }
 
@@ -12,63 +11,180 @@ const renderLicenseBadge = (license) => {
   `;
 }
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-const renderLicenseLink = (license) => { 
-  if (!license){
+//generate license text
+const renderLicenseSection = (license, githubUsername) => {
+  if (!license) {
     return '';
   }
-}
+  else if (license === 'MIT') {
+    return `
+    MIT License
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-const renderLicenseSection = (license) => { 
-  if (!license){
+    Copyright (c) ${new Date().getFullYear()} ${githubUsername}
+    
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+    
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+    
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+    `;
+  }
+  else if (license === 'Unlicense') {
+    return `
+    This is free and unencumbered software released into the public domain.
+
+    Anyone is free to copy, modify, publish, use, compile, sell, or
+    distribute this software, either in source code form or as a compiled
+    binary, for any purpose, commercial or non-commercial, and by any
+    means.
+
+    In jurisdictions that recognize copyright laws, the author or authors
+    of this software dedicate any and all copyright interest in the
+    software to the public domain. We make this dedication for the benefit
+    of the public at large and to the detriment of our heirs and
+    successors. We intend this dedication to be an overt act of
+    relinquishment in perpetuity of all present and future rights to this
+    software under copyright law.
+
+      THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+    MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+    IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+    OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+    ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+    OTHER DEALINGS IN THE SOFTWARE.
+
+    For more information, please refer to <https://unlicense.org>
+    `;
+  }
+};
+
+//function to generate the table of contents
+const generateTableOfContents = (description, license, questions, installation, collaborators, badges,tests) => {
+  let existingVariables = generateTableOfContents.filter(Boolean);
+  existingVariables.forEach(item => {
+    item[0].toUpperCase();
+    return `* [${item}](#${item})`;
+  });
+  return existingVariables;
+};
+
+//function to generate installation guidelines
+const generateInstallation = (installation) => {
+  if (!installation){
     return '';
   }
-}
+  return `
+    ## Installation
+    
+    ### Follow these instructions to install the project
+    - ${installation}
+    `;
+};
 
-//TODO: create a function to generate the table of contents
-//TODO: create a function to generate installation guidelines
-//TODO: create a function to generate usage section
+//function to generate usage section
+const generateUsage = usage => {
+  if (!usage){
+    return '';
+  }
+  return `
+  ## Usage
+    
+  ### Follow these instructions to use the project
+  - ${usage}
+  `;
+};
+
 //TODO: create a function to generate the questions section
+const generateQuestions = (questions, questionsDescription, githubUsername) => {
+  if (!questions){
+    return '';
+  }
+  return `
+  ## Questions
+    
+  ### Contact me
+  #### GitHub:
+  - ${githubUsername}
+  #### Email:
+  - _${questions}_
+  Instructions:
+  -${questionsDescription}
+  `;
+};
+
 //TODO: create a function to generate the credits section
+const generateCredits = credits => {
+  if (!credits){
+    return '';
+  }
+  return `
+  ## Credits
+  ### Project Collaborators
+  - ${credits}
+  `;
+};
+
 //TODO: creata a function to generate the badges section
+const generateBadges = (badges, githubUsername, githubRepo) => {
+  if (!badges){
+    return '';
+  }
+  return `
+  ## Badges
+
+  ![GitHub last commit](https://img.shields.io/github/last-commit/${githubUsername}/${githubRepo})
+  ![GitHub repo size](https://img.shields.io/github/repo-size/${githubUsername}/${githubRepo})
+  ![GitHub issues](https://img.shields.io/github/issues/${githubUsername}/${githubRepo})
+  ![GitHub top language](https://img.shields.io/github/languages/top/${githubUsername}/${githubRepo}) ![GitHub language count](https://img.shields.io/github/languages/count/${githubUsername}/${githubRepo})
+  `;
+};
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(templateData) {
   //destructure template data from questions
-  const { title, license, questions, installation, usage, credits, badges, contributing } = templateData;
+  // const { title, license, questions, installation, usage, credits, badges, contributing } = templateData;
+  const { title, description, githubRepo, githubUsername, license, questions, questionsDescription, installation, collaborators, badges, tests } = templateData;
 
   return `
   # ${title}
+
+  ## Table of Contents
+
+  ${generateTableOfContents(description, license, questions, installation, collaborators, badges,tests)}
 
   ## Description
 
   ${description}
   ${renderLicenseBadge(license)}
 
-  ## Table of Contents
-
-  ${generateTableOfContents(templateData)}
-
   ${generateInstallation(installation)}
 
   ${generateUsage(usage)}
 
-  ${renderLicenseSection(license)}
-
-  ${renderLicenseLink(license)}
-
-  ${generateQuestions(questions)}
+  ${generateQuestions(questions, questionsDescription, githubUsername)}
 
   ${generateCredits(credits)}
 
-  ${generateBadges(badges)}
+  ${generateBadges(badges, githubUsername, githubRepo)}
+
+  ${renderLicenseSection(license)}
 
   ${generateContributing(contributing)}
 
-  ### MADE WITH README GENERATOR
+  ### _MADE WITH README GENERATOR_
   ![Developer Tag](https://img.shields.io/badge/Developed%20By%3A-Zack%20Anderson-orange)
   ![GitHub followers](https://img.shields.io/github/followers/zackaryanderson?style=social)
         
